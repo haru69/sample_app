7aeed35d9894c85c91af54076b0c33fb11c0f192
haru69 <haruki.footboll.boy@gmail.com>
2021-01-02 18:40:17 +0900

    signup test generate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:id])
+     if @user.save
+     
+    else
+      render"new"
+     end
+  end
+
+private
+
+
+def user_params 
+  params.require(:user).permit(:name,:email,:password,:password_confimation)
+end
+
+  
+
 end
